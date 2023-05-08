7c5cf99767aac3eeb0f2cd5db6745e41c825bfe0
Sohel <sohel@simformsolutions.com>
2023-05-08 09:55:31 +0530

    util Added

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 const express=require('express')
 const app=express();
+const inquirer = require("inquirer");
+const validation = require("./util/validationTime");
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "weekday",
+      message: "Choose Day you want to Search :",
+      choices:['Mon','Tue','Wed',"Thu","Fri","Sat"]
+    },
+    {
+      type: "input",
+      name: "time",
+      message: "Enter time (HH:MM AM/PM): ",
+    },
+  ])
+  .then((answer) => {
+    let Week_Day = answer.weekday
+    let Day_Time = answer.time;
+
+    if (!validation(Day_Time)) {
+      throw new Error("PLease Enter Valid Time");
+    }
+    const Check_Shop=
+    console.log(ConvertedTime);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
