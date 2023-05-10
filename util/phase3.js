8357b598b47989593f04a3e4b01a2da22f018e3c
Sohel <sohel@simformsolutions.com>
2023-05-10 12:29:25 +0530

    code optimize

--- util/phase3.js
+++ util/phase3.js
@@ -6,66 +6,46 @@ const openCloseTime = (Week_Day, Day_Time) => {
 
   const check_Time = new Date(`01/01/2001 ${Day_Time}`);
 
+  const calculation=(a,b)=>{
+    const totalmilis =  a.getTime()- b.getTime();
+    const minute = Math.floor(totalmilis / (60 * 1000)) % 60;
+    const hour = Math.floor(totalmilis / (60 * 60 * 1000));
+    return [minute, hour]
+  }
 // If Day is not Available
   if (!Find_day) {
     const next_open=next_day(Week_Day)
-    const totalmilis =  next_open.getTime()- check_Time.getTime();
-    const minute = Math.floor(totalmilis / (60 * 1000)) % 60;
-    const hour = Math.floor(totalmilis / (60 * 60 * 1000));
+    const [minute,hour]=calculation(next_open,check_Time)
     let days = parseInt(hour / 24);
-    let remain_hours = hour - days * 24;
-    return (
-      `Shop will open after${ days==0? "":" "+days}${days==1?" day ":""}${remain_hours} hour and ${minute} minutes 😢`
-    );
+    let remaining_hours = hour - days * 24;
+    return (`Shop will open after${ days==0? "":" "+days}${days==1?" day ":""}${remaining_hours} hour and ${minute} minutes 😢`);
   }
 
   const open_Time = new Date(`01/01/2001 ${Find_day.open}`);
-  console.log(open_Time);
   const close_Time = new Date(`01/01/2001 ${Find_day.close}`);
-  console.log(close_Time);
 
 //  If Check Time is Earliar
   if (check_Time <= open_Time) {
-    const totalmilis = open_Time.getTime() - check_Time.getTime();
-    const minute = Math.floor(totalmilis / (60 * 1000)) % 60;
-    const hour = Math.floor(totalmilis / (60 * 60 * 1000)) % 24;
-    return (
-      "Shop will open after " + hour + " hours and " + minute + " minutes 😢"
-    );
+    const [minute,hour]=calculation(open_Time,check_Time)
+    return ("Shop will open after " + hour + " hours and " + minute + " minutes 😢");
   } 
 //  If Shop is Close on Check Time
   else if (check_Time >= close_Time) {
     const next_open=next_day(Week_Day)
-    console.log(next_open,"nextOpen");
-    const totalmilis = next_open.getTime() - check_Time.getTime();
-    const minute = Math.floor(totalmilis / (60 * 1000)) % 60;
-    const hour = Math.floor(totalmilis / (60 * 60 * 1000));
+    const [minute,hour]=calculation(next_open,check_Time)
     let days = parseInt(hour / 24);
-    let remain_hours = hour - days * 24;
-    return (
-      `Shop will open after${ days==0? "":" "+days}${days==1|days==2?" day ":" "}${remain_hours} hour and ${minute} minutes 😢`
-        );
+    let remaining_hours = hour - days * 24;
+    return (`Shop will open after${ days==0? "":" "+days}${days==1|days==2?" day ":" "}${remaining_hours} hour and ${minute} minutes 😢`);
   } 
+
 //  If Shop is Open
   else {
-    const totalmilis = close_Time.getTime() - check_Time.getTime();
-    const minute = Math.floor(totalmilis / (60 * 1000)) % 60;
-    const hour = Math.floor(totalmilis / (60 * 60 * 1000)) % 24;
-    return (
-      "Shop will close after " + hour + " hours and " + minute + " minutes 😍"
-    );
+    const [minute,hour]=calculation(close_Time,check_Time)
+    return ("Shop will close after " + hour + " hours and " + minute + " minutes 😍");
   }
 };
 
-let week_days = [
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat",
-  "Sun",
-];
+let week_days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun",];
 const next_day = (inday) => {
     let index=week_days.findIndex((day)=>day===inday)
     console.log(index);
@@ -77,7 +57,7 @@ const next_day = (inday) => {
           console.log(obj.day,week_days[(index+i)%7],index,i);
           return obj.day===(week_days[(index+i)%7])
         })
-        console.log(status,"status");
+        console.log("===========");
         if(status){
             break;
         }
@@ -86,4 +66,5 @@ const next_day = (inday) => {
     console.log(next_date);
     return next_date
 };
+
 module.exports = openCloseTime;
